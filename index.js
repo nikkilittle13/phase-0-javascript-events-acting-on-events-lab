7462d44d56bc1cbeddc2ab99847b6b3052646d1d
Nikki Little <nikkilittle13@gmail.com>
2023-10-05 20:51:09 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const dodger = document.getElementById("dodger");
+
 function moveDodgerLeft() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
@@ -7,11 +9,23 @@ function moveDodgerLeft() {
   }
 }
 
-function moveDodgerRight() {
-  const leftNumbers = dodger.style.left.replace("px", "");
-  const left = parseInt(leftNumbers, 10);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
 
-  if (left > 0) {
-    dodger.style.left = `${left + 1}px`;
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const right = parseInt(rightNumbers, 10);
+
+  if (right > 0) {
+    dodger.style.left = `${right + 1}px`;
   }
 }
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
